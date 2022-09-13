d566bf64a94b1b24fc3a6ccdc0cfd9a7edb7b95b
cubikean <60139156+cubikean@users.noreply.github.com>
2022-09-13 11:06:36 +0200

    ADD PHP

---
+++ MiseaNiveau/PHP/Manip1_Php_SiteACompleter/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Les papas heureux</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/mediumSize.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+</head>
+
+<body>
+    <?php
+        include("elements/menu.html") ;
+    ?>
+    
+    <main>
+        <header>
+            <h1>Les papas heureux</h1>
+
+        </header>
+        <article>
+            <h2>Présentation</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum a quos sit repellendus aspernatur repellat corporis rem, numquam quis. Quaerat, in, ullam aspernatur quis ut perferendis similique libero. Neque, atque.</p>
+            <p>A quia officiis pariatur voluptatem tenetur itaque eaque, praesentium autem natus. Dicta voluptas, fuga quia repudiandae consequuntur officiis molestias rerum at ullam ea blanditiis soluta laborum ipsum quas, cumque dignissimos.</p>
+            <p>Est dolorum odio, animi assumenda. Quam nulla reprehenderit commodi aperiam hic dolorem illum voluptas autem quas earum, provident obcaecati minus accusantium quaerat error laudantium ullam vero nihil esse incidunt alias.</p>
+            <p>A quia officiis pariatur voluptatem tenetur itaque eaque, praesentium autem natus. Dicta voluptas, fuga quia repudiandae consequuntur officiis molestias rerum at ullam ea blanditiis soluta laborum ipsum quas, cumque dignissimos.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum a quos sit repellendus aspernatur repellat corporis rem, numquam quis. Quaerat, in, ullam aspernatur quis ut perferendis similique libero. Neque, atque.</p>
+        </article>
+
+    </main>
+    <footer>
+        <p>Copyright P. Rodriguez</p>
+        <p>Copyright Cubikon</p>
+        <p><a href="www.univ-larochelle.fr">Université de La Rochelle</a></p>
+    </footer>
+</body>
+
+</html>
