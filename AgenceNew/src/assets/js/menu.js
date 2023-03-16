2d9e38765f23985e7b22facaf67110981e6d6a47
cubikean <hugochatigny@gmail.com>
2023-03-16 23:12:14 +0100

    Update du site agence

--- AgenceNew/src/assets/js/menu.js
+++ AgenceNew/src/assets/js/menu.js
@@ -1,9 +1,12 @@
 console.log('here');
 
-const btnMenu = document.querySelector('.main-menu')
+const btnMenu = document.querySelector('.main-menu_toggler')
+const menu = document.querySelector('.main-menu')
+const ulActive = document.querySelector('.main-menu_list')
 
 btnMenu.addEventListener('click', ()=>{
 
-    btnMenu.classList.toggle('main-menu--active')
+    menu.classList.toggle('main-menu--active')
+    ulActive.classList.toggle('main-menu_list--active')
 
 })
