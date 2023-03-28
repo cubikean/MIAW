b1fa7e8ed6e00e7c20e41a93811bc2190d3cda1f
Hugo Chatigny <hugochatigny@gmail.com>
2023-03-28 09:38:24 +0200

    Update de l'agenceNew

--- AgenceNew/src/assets/js/menu.js
+++ AgenceNew/src/assets/js/menu.js
@@ -3,6 +3,7 @@ console.log('here');
 const btnMenu = document.querySelector('.main-menu_toggler')
 const menu = document.querySelector('.main-menu')
 const ulActive = document.querySelector('.main-menu_list')
+const listItems = document.querySelectorAll('.main-menu_list_item')
 
 btnMenu.addEventListener('click', ()=>{
 
@@ -10,3 +11,19 @@ btnMenu.addEventListener('click', ()=>{
     ulActive.classList.toggle('main-menu_list--active')
 
 })
+
+
+if (window.innerWidth <= 768) {
+    listItems.forEach(item => {
+        item.addEventListener('click', ()=>{
+            menu.classList.toggle('main-menu--active')
+            ulActive.classList.toggle('main-menu_list--active')
+        })
+    });
+}
+
+function getCurrentYear() {
+    return new Date().getFullYear(); // returns the year via local timing
+  };
+
+document.querySelector('.year-date').innerHTML = `© ${getCurrentYear()}`;
