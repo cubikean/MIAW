99fa9353c85cb98185059bb4f768a58347ec6ade
Hugo Chatigny <hugochatigny@gmail.com>
2023-04-04 14:00:16 +0200

    change repo eval

--- Cours/WordPress/revision/functions/load-style.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-function load_style() {
-
-	wp_enqueue_style('fontawesome_css', get_template_directory_uri() . '/assets/css/fontawesome-all.min.css' );
-	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/assets/css/main.css' );
-
-}
-add_action('wp_enqueue_scripts', 'load_style');
--- Cours/WordPress/revision/searchform.php
+++
@@ -1,7 +0,0 @@
-<form action="<?php echo home_url( '/' ); ?>" method="get">
-    <label for="search">Recherche : </label>
-
-    <input type="hidden" value="slider" name="post_type">
-    <input type="text" name="s" value="<?php the_search_query(); ?>">
-    <button type="submit">Rechercher</button>
-</form>
